abc9cfbb4e9e99a5137bab1082c5a5cd5c2cd0bb
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-02-09 10:30:08 -0600

    Initial Commit

---
+++ Za-Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za-Hunter
+//
+//  Created by stephanie rocha marquez on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
